2a6220c6755557442b8dbdeea7578e747316fa52
jevans <devnull@localhost>
2003-10-04 22:32:39 +0000

    Begin restructuring the tree code such that there are actual tree objects, rather than just tree nodes.

--- trunk/cruxroot/crux/include/tr.h
+++ trunk/cruxroot/crux/include/tr.h
@@ -11,9 +11,11 @@
  ******************************************************************************/
 
 typedef struct cw_trn_s cw_trn_t;
+typedef struct cw_trt_s cw_trt_t;
+typedef struct cw_tr_s cw_tr_t;
 
 /* Canonical unrooted bifurcating phylogenetic tree in compact form. */
-typedef cw_uint8_t cw_tr_t;
+typedef cw_uint8_t cw_trs_t;
 
 /* Tree node for an unrooted bifurcating phylogenetic tree. */
 struct cw_trn_s
@@ -21,6 +23,9 @@ struct cw_trn_s
 #ifdef CW_DBG
     cw_uint32_t magic;
 #define CW_TRN_MAGIC 0x63329478
+    /* If this trn has been inserted into a tree, it should no longer be
+     * externally modified.  This variable aids in asserting this invariant. */
+    cw_bool_t in_tr;
 #endif
     /* If non-NULL, then the node was dynamically allocated. */
     cw_mema_t *mema;
@@ -40,93 +45,201 @@ struct cw_trn_s
 #define CW_TRN_EDGE_NONE 0xffffffffU
 };
 
-/* tr. */
-cw_tr_t *
-tr_new(cw_mema_t *a_mema, cw_trn_t *a_trn, cw_uint32_t a_ntaxa);
+/* TBR neighbor. */
+struct cw_trt_s
+{
+    /* Number of neighbors that can be reached by doing TBR at edges before this
+     * one.  This is also the neighbor number of the first neighbor that can be
+     * reached by doing TBR on this edge. */
+    cw_uint32_t offset;
 
-void
-tr_delete(cw_tr_t *a_tr, cw_mema_t *a_mema, cw_uint32_t a_ntaxa);
+    /* Number of edges in the two subtrees.  Note that 0 and 1 are different
+     * logical cases, but the number of connections possible for those two cases
+     * is the same. */
+    cw_uint32_t nedges_a;
+    cw_uint32_t nedges_b;
+};
 
-cw_uint32_t
-tr_ntaxa(const cw_tr_t *a_tr);
+struct cw_tr_s
+{
+#ifdef CW_DBG
+    cw_uint32_t magic;
+#define CW_TR_MAGIC 0x39886394
+#endif
+    /* If non-NULL, then the node was dynamically allocated. */
+    cw_mema_t *mema;
 
-cw_uint32_t
-tr_ntaxa2sizeof(cw_uint32_t a_ntaxa);
+    /* Auxiliary opaque data pointer.  This is used by the treenode wrapper code
+     * for reference iteration. */
+    void *aux;
 
-void
-tr_memcopy(cw_uint8_t *a_dest, cw_tr_t *a_tr);
+    /* Number of taxa in tree. */
+    cw_uint32_t ntaxa;
 
-cw_trn_t *
-tr_trn(cw_tr_t *a_tr, cw_mema_t *a_mema, cw_uint32_t a_ntaxa);
+    /* TRUE if this is a rooted tree, false otherwise. */
+    cw_bool_t rooted;
 
-cw_uint32_t
-tr_hash(const void *a_key);
+    /* Root node.  If this tree is unrooted, the root node has no neighbors. */
+    cw_trn_t *root;
 
-cw_bool_t
-tr_key_comp(const void *a_k1, const void *a_k2);
+    /* Taxon 0. */
+    cw_trn_t *croot;
+
+    /* Undo information for the most recent TBR. */
+    cw_bool_t tbr_undoable;
+    cw_uint32_t tbr_undo_bisect;
+    cw_uint32_t tbr_undo_edge_a;
+    cw_uint32_t tbr_undo_edge_b;
+
+    /* Array of triplets that store information that is used for TBR-related
+     * functions. */
+    cw_trt_t *trt;
+};
 
 /* trn. */
+
+/* Constructor. */
 cw_trn_t *
 trn_new(cw_trn_t *a_trn, cw_mema_t *a_mema);
 
+/* Destructor. */
 void
 trn_delete(cw_trn_t *a_trn);
 
+/* Get the taxon number associated with a_trn.  Return CW_TRN_TAXON_NONE if no
+ * taxon number is set. */
 cw_uint32_t
 trn_taxon_num_get(cw_trn_t *a_trn);
 
+/* Set the taxon number associated with a_trn (use CW_TRN_TAXON_NONE to unset
+ * the taxon number. */
 void
 trn_taxon_num_set(cw_trn_t *a_trn, cw_uint32_t a_taxon_num);
 
+/* Get neighbor a_i of the trn. */
 cw_trn_t *
 trn_neighbor_get(cw_trn_t *a_trn, cw_uint32_t a_i);
 
+/* Swap two neighbors of a trn. */
 void
 trn_neighbors_swap(cw_trn_t *a_trn, cw_uint32_t a_i, cw_uint32_t a_j);
 
+/* Join two trn's. */
 void
 trn_join(cw_trn_t *a_a, cw_trn_t *a_b);
 
+/* Detatch two trn's. */
 void
 trn_detach(cw_trn_t *a_a, cw_trn_t *a_b);
 
+/* Get the value of the auxiliary pointer associated with the trn. */
 void *
 trn_aux_get(cw_trn_t *a_trn);
 
+/* Set the value of the auxiliary pointer associated with the trn. */
 void
 trn_aux_set(cw_trn_t *a_trn, void *a_aux);
 
-/* Functions that operate on trn-based trees. */
-cw_trn_t *
-trn_tree_random(cw_mema_t *a_mema, cw_uint32_t a_ntaxa);
+/* tr. */
 
+/* Constructor. */
+cw_tr_t *
+tr_new(cw_tr_t *a_tr, cw_mema_t *a_mema, cw_trn_t *a_trn);
+
+/* Destructor. */
 void
-trn_tree_delete(cw_trn_t *a_trn);
+tr_delete(cw_tr_t *a_tr);
 
+/* Get the number of taxa in the tree. */
 cw_uint32_t
-trn_tree_ntaxa_get(cw_trn_t *a_trn);
+tr_ntaxa_get(cw_tr_t *a_tr);
 
+/* Get the number of edges in the tree. */
 cw_uint32_t
-trn_tree_nedges_get(cw_trn_t *a_trn);
+tr_nedges_get(cw_tr_t *a_tr);
 
+/* Get edge a_edge. */
+void
+tr_edge_get(cw_tr_t *a_tr, cw_uint32_t a_edge, cw_trn_t **r_trn,
+	    cw_uint32_t *r_neighbor);
+
+/* Get the edge index of the edge between two trn's. */
+cw_uint32_t
+tr_edge_index_get(cw_tr_t *a_tr, cw_trn_t *a_trn_a, cw_trn_t *a_trn_b);
+
+/* Get the root of the tree, were this a canonical tree (may or may not be).
+ * This is always the lowest numbered taxon node. */
 cw_trn_t *
-trn_tree_root_get(cw_trn_t *a_trn);
+tr_croot_get(cw_tr_t *a_tr);
 
-void
-trn_tree_edge_get(cw_trn_t *a_trn, cw_uint32_t a_edge, cw_trn_t **r_trn,
-		  cw_uint32_t *r_neighbor);
+/* Get the root of the tree.  If the tree is rooted, this node will not have
+ * any neighbors. */
+cw_trn_t *
+tr_root_get(cw_tr_t *a_tr);
 
-void
-trn_tree_canonize(cw_trn_t *a_trn);
+/* Return whether the tree is rooted. */
+cw_bool_t
+tr_rooted(cw_tr_t *a_tr);
 
+/* Root the tree at the edge between a_trn and neighbor a_neighbor. */
 void
-trn_tree_bisect(cw_trn_t *a_trn, cw_uint32_t a_edge, cw_trn_t **r_trn_a,
-		cw_uint32_t *r_edge_a, cw_trn_t **r_trn_b,
-		cw_uint32_t *r_edge_b, cw_trn_t **r_spare_a,
-		cw_trn_t **r_spare_b);
+tr_root(cw_tr_t *a_tr, cw_trn_t *a_trn, cw_uint32_t a_neighbor);
 
+/* Unroot the tree. */
 void
-trn_tree_connect(cw_trn_t *a_trn_a, cw_uint32_t a_edge_a,
-		 cw_trn_t *a_trn_b, cw_uint32_t a_edge_b,
-		 cw_trn_t **ar_spare_a, cw_trn_t **ar_spare_b,
-		 cw_trn_t **r_trn, cw_uint32_t *r_edge);
+tr_unroot(cw_tr_t *a_tr);
+
+/* Canonize the tree.  The tree must be unrooted. */
+void
+tr_canonize(cw_tr_t *a_tr);
+
+/* Perform TBR. */
+void
+tr_tbr(cw_tr_t *a_tr, cw_uint32_t a_bisect, cw_uint32_t a_reconnect_a,
+       cw_uint32_t a_reconnect_b, cw_uint32_t *r_bisect,
+       cw_uint32_t *r_reconnect_a, cw_uint32_t *r_reconnect_b);
+
+/* Undo the previous TBR.  Using this rather than manually reversing the
+ * previous TBR allows internal caching of neighbors to still be used. */
+void
+tr_tbr_undo(cw_tr_t *a_tr);
+
+/* Get the number of neighboring trees reachable via TBR. */
+cw_uint32_t
+tr_tbr_nneighbors_get(cw_tr_t *a_tr);
+
+/* Get the parameters necessary to transorm this tree to neighbor a_neighbor. */
+void
+tr_tbr_neighbor_get(cw_tr_t *a_tr, cw_uint32_t a_neighbor,
+		    cw_uint32_t *r_bisect, cw_uint32_t *r_reconnect_a,
+		    cw_uint32_t *r_reconnect_b);
+
+/* trs. */
+
+/* Constructor. */
+cw_trs_t *
+trs_new(cw_mema_t *a_mema, cw_tr_t *a_tr);
+
+/* Destructor. */
+void
+trs_delete(cw_trs_t *a_trs, cw_mema_t *a_mema, cw_uint32_t a_ntaxa);
+
+/* Get the number of taxa in the tree. */
+cw_uint32_t
+trs_ntaxa(const cw_trs_t *a_trs);
+
+/* Get the size (in bytes) of a trs with a_ntaxa taxa. */
+cw_uint32_t
+trs_ntaxa2sizeof(cw_uint32_t a_ntaxa);
+
+/* Copy a_trs to a string. */
+void
+trs_memcopy(cw_uint8_t *a_dest, cw_trs_t *a_trs);
+
+/* Hash a trs. */
+cw_uint32_t
+trs_hash(const void *a_key);
+
+/* Compare two trs's. */
+cw_bool_t
+trs_key_comp(const void *a_k1, const void *a_k2);
