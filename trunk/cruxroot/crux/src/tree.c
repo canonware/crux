4740245dd1760f20d96b7f9933a4a68d5d78e76a
jevans <devnull@localhost>
2004-06-30 20:58:14 +0000

    Fix reference counting problems in tree_canonize().

--- trunk/cruxroot/crux/src/tree.c
+++ trunk/cruxroot/crux/src/tree.c
@@ -234,7 +234,27 @@ tree_canonize(TreeObject *self)
     xep_begin();
     xep_try
     {
+	cw_tr_node_t old_tr_node, tr_node;
+	NodeObject *node;
+
+	old_tr_node = tr_base_get(self->tr);
+
 	tr_canonize(self->tr);
+
+	/* Reference new base. */
+	tr_node = tr_base_get(self->tr);
+	if (tr_node != CW_TR_NODE_NONE)
+	{
+	    node = (NodeObject *) tr_node_aux_get(self->tr, tr_node);
+	    Py_INCREF(node);
+	}
+
+	/* Decref old base. */
+	if (old_tr_node != CW_TR_NODE_NONE)
+	{
+	    node = (NodeObject *) tr_node_aux_get(self->tr, old_tr_node);
+	    Py_DECREF(node);
+	}
 
 	Py_INCREF(Py_None);
 	retval = Py_None;
