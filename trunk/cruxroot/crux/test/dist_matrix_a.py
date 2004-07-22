07ef799a227a6bd9094abee798383eae334a1f9c
jasone <devnull@localhost>
2004-07-22 19:15:55 +0000

    Clean up iteration code.

--- trunk/cruxroot/crux/test/dist_matrix_a.py
+++ trunk/cruxroot/crux/test/dist_matrix_a.py
@@ -21,41 +21,41 @@ def matrix_print(matrix, ntaxa):
         sys.stdout.write("\n")
         x += 1
 
-matrix_0 = """     5
+matrices = ["""     5
 Alpha      0.000 1.000 2.000 3.000 3.000
 Beta       1.000 0.000 2.000 3.000 3.000
 Gamma      2.000 2.000 0.000 3.000 3.000
 Delta      3.000 3.000 0.000 0.000 1.000
 Epsilon    3.000 3.000 3.000 1.000 0.000
-"""
+""",
 
-matrix_1 = """5
+"""5
 Taxon_A 0.0 1.0 2.0 3.0 4.0
 Taxon_B 1.0 0.0 1.5 2.5 3.5
 Taxon_C 2.0 1.5 0.0 2.2 3.2
 Taxon_D 3.0 2.5 2.2 0.0 3.1
 Taxon_E 4.0 3.5 3.2 3.1 0.0
-"""
+""",
 
-matrix_2 = """5
+"""5
 Taxon_A     1.0 2.0 3.0 4.0
 Taxon_B         1.5 2.5 3.5
 Taxon_C             2.2 3.2
 Taxon_D                 3.1
 Taxon_E
-"""
+""",
 
-matrix_3 = """5
+"""5
 Taxon_A
 Taxon_B 1.0
 Taxon_C 2.0 1.5
 Taxon_D 3.0 2.5 2.2
 Taxon_E 4.0 3.5 3.2 3.1
-"""
+"""]
 
 print "Test begin"
 
-for matrix in (matrix_0, matrix_1, matrix_2, matrix_3):
+for matrix in matrices:
     (map, matrix) = crux.dist_matrix().parse(matrix)
 
     print map.taxa_get()
