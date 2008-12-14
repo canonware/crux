0b633b6efca23e6fd1be0a2e720fe176d9a1632b
Jason Evans <jasone@canonware.com>
2008-12-13 19:59:53 -0800

    Move DistMatrixLexer-specific declarations into the correct header.

--- crux/lib/DistMatrix/CxDistMatrix.h
+++ crux/lib/DistMatrix/CxDistMatrix.h
@@ -6,36 +6,6 @@
 typedef float CxtDMDist;
 typedef unsigned long CxtDMSize;
 
-typedef enum {
-    CxeDistMatrixLexerInputModeFd,
-    CxeDistMatrixLexerInputModeStr
-} CxtDistMatrixLexerInputMode;
-
-typedef enum {
-    CxeDistMatrixLexerTokTypeInt,
-    CxeDistMatrixLexerTokTypeDist,
-    CxeDistMatrixLexerTokTypeLabel
-} CxtDistMatrixLexerTokType;
-
-typedef struct {
-    unsigned line;
-    int ioerror;
-    CxtDistMatrixLexerInputMode inputMode;
-    union {
-        int fd;
-        struct {
-            char *s;
-            size_t len;
-        } s;
-    } input;
-
-    CxtDistMatrixLexerTokType tokType;
-    union {
-        CxtDMDist dist;
-        char *label;
-    } tok;
-} CxtDistMatrixLexerExtra;
-
 #ifndef CxmUseInlines
 CxtDMSize
 CxDistMatrixNxy2i(CxtDMSize n, CxtDMSize x, CxtDMSize y);
